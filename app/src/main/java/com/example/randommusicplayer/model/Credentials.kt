0437b4676891a99ce22660d78b0094ae64a9beb0
Naveen Kumar Singh <60296575+Naveens1605@users.noreply.github.com>
2021-08-28 20:49:01 +0530

    Update Credentials.kt

--- app/src/main/java/com/example/randommusicplayer/model/Credentials.kt
+++ app/src/main/java/com/example/randommusicplayer/model/Credentials.kt
@@ -2,7 +2,7 @@ package com.example.randommusicplayer.model
 
 enum class Credentials(private val credentials: String) {
 
-    CLIENT_ID("c73b5ec105894fa4b2c950879ab6872b"),
+    CLIENT_ID("YOUR CLIENT ID"),
     REDIRECT_URI("com.example.randommusicplayer://callback"),
     SCOPE("user-read-email,user-read-private,user-library-modify,user-library-read,user-follow-modify,user-follow-read,playlist-modify-public,playlist-modify-private,playlist-read-private,playlist-read-collaborative,user-read-recently-played,user-top-read,user-read-playback-position,user-read-recently-played,user-top-read,user-read-playback-position");
 
